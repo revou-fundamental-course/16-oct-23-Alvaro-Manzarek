932c67b8d836a63b9c2ff6bd2621381b8a0c916a
Alvaro <alvaromanzarek@gmail.com>
2023-10-26 23:13:09 +0700

    kelar

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function displayGreeting(){
+    let name = document.getElementById(nama).value;
+    document.getElementById(outputDiv).innerHTML = `Hello` + name +
+      `Welcome to Livewell page` +
+      `
+      You are officially a livewell member` + name + `!`;
+    alert(alerr)
+  }
